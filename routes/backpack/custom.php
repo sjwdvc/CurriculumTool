40020e38718311ddefaef5c9383ca1e4851d74f6
Stefano Verhoeve <sverhoeve@davinci.nl>
2020-01-24 15:45:37 +0100

    removed /admin prefix for amdin page

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -7,7 +7,7 @@
 // Routes you generate using Backpack\Generators will be placed here.
 
 Route::group([
-    'prefix'     => config('backpack.base.route_prefix', 'admin'),
+    'prefix'     => config('backpack.base.route_prefix', ''),
     'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
